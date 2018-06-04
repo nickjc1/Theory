2a6f9bb5aa6f1a815c4f03709ff86cfc2b7ac53d
CHAO JIANG <cjiang@CHAOs-MacBook-Pro.local>
2018-06-04 15:12:32 -0500

    updated persistent data method to using coredata, and start using CRUD

--- Todoey/Controller/TodoLIstViewController.swift
+++ Todoey/Controller/TodoLIstViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: UITableViewController, UISearchBarDelegate {
 
     //Declare veriable here:
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
+    
     var itemArray = [Item]()
+    let theContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // Do any additional setup after loading the view, typically from a nib.
-//         print(dataFilePath)
+        
+        //find the path to where the data is stored in your device:
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
+        
+        
     }
 
     //MARK - TableView DataSource Methods:
@@ -34,11 +40,6 @@ class TodoListViewController: UITableViewController {
         
         //ternery operator to rewrite code:
         cell.accessoryType = item.isChecked ? .checkmark : .none
-//        if itemArray[indexPath.row].isChecked {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
         
         return cell;
     }
@@ -52,11 +53,19 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        itemArray[indexPath.row].isChecked = !itemArray[indexPath.row].isChecked
+//        theContext.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
+        //update ischecked attribute method1:
+        let ischecked : Bool = itemArray[indexPath.row].isChecked
+        itemArray[indexPath.row].setValue(!ischecked, forKey: "isChecked")
+
+        //update ischecked attribute method2:
+//      itemArray[indexPath.row].isChecked = !itemArray[indexPath.row].isChecked
         
         tableView.deselectRow(at: indexPath, animated: true)
-        
         saveItems()
+        
     }
     
     //MARK - addButton:
@@ -69,8 +78,9 @@ class TodoListViewController: UITableViewController {
         
         let addItemAction = UIAlertAction(title: "Add Item", style: .default) {(action) in
             
-            let item = Item()
+            let item = Item(context: self.theContext)
             item.title = textField.text!
+            item.isChecked = false
             self.itemArray.append(item)
             
             self.saveItems()
@@ -96,28 +106,24 @@ class TodoListViewController: UITableViewController {
     
     //save items method:
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try theContext.save()
         } catch {
-            print("error encoding item array \(error)")
+            print("error save item array \(error)")
         }
-        
+       
         self.tableView.reloadData()
     }
     
     func loadItems() {
-        
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            }catch {
-                print("error decoding item array \(error)")
-            }
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try theContext.fetch(request)
+        } catch {
+            print("error fetch item array \(error)")
         }
+        
     }
     
 
