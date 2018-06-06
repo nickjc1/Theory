7d10e7fe3199dcae8953f1b05fa9ed1131eee861
CHAO JIANG <cjiang@CHAOs-MacBook-Pro.local>
2018-06-06 16:32:14 -0500

    added new categorytableview into project; finished setting up categorytableview database and add-new functionality

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,92 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by CHAO JIANG on 6/6/18.
+//  Copyright © 2018 nickjc1. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    // MARK: - Declare veriable here:
+    
+    var categoryArray : [Category] = []
+    let theContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+
+    // MARK: - viewDidLoad()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadCategory()
+    }
+    
+    // MARK: - tableview delegate and tableview datasource method:
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        cell.textLabel?.text = categoryArray[indexPath.row].name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    // MARK: - addbuttonPressed method:
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Cotegory", message: "", preferredStyle: .alert)
+        let addItemAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            let theCategory = Category(context: self.theContext)
+            let categoryName = textField.text
+            theCategory.name = categoryName
+            self.categoryArray.append(theCategory)
+            
+            self.saveCategory()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("dismiss")
+        }
+        
+        alert.addAction(addItemAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Model Manipulation methods:
+    
+    func saveCategory() {
+        do{
+            try theContext.save()
+        }catch{
+            print("error save category")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categoryArray = try theContext.fetch(request)
+        } catch {
+            print("error fetch category array \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+
+}
